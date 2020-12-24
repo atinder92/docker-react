b5a70760b2e5f0dfee0534b2ad05eb59b83855ed
Atinder Singh <atindersingh@Atinders-MacBook-Pro.local>
2020-12-25 02:48:53 +1100

    updated app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Learning docker</p>
+      <p>Learning docker.</p>
+      <p>Application development using react.</p>
     </div>
   );
 }
