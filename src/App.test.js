82c20438e6d94d1e158d957e0cb41fe8e36c1a9e
Atinder Singh <atindersingh@Atinders-MacBook-Pro.local>
2020-12-25 02:06:34 +1100

    initial setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Learning docker on screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/Learning docker/i);
 });
